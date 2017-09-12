73a90115202086b1de045041288adb440bd158df
I-am-Gabi <gabicavalcantesilva@gmail.com>
2017-09-12 20:09:31 -0300

    total e grafico

--- etnia.py
+++ etnia.py
@@ -9,18 +9,29 @@ import csv
 
 header = []
 content = []
+total = []
 
 with open('dadosEtnicos.csv', newline='', encoding='utf_8') as f: 
     reader = csv.reader(f) 
     for row in reader:
     	line = row[0].split(';')
-    	header.append(line[0:1][0]) 
+    	header.append(line[0:1][0])
+    	l = []
     	for cell in line[1:]:
-    		content.append(cell)  
+    		l.append(cell)  
+    	content.append(l)
+    	if (l[-1] != 'Total'):
+    		total.append(float(l[-1]))
 
 
-print(header)
-print(content)
+#print(total)
+#print(header[1:])
+plt.bar([0, 1, 2, 3, 4, 5, 6, 7],  total,0.5, align='center')
+#plt.xticks(total, header[1:])
+plt.yticks([0, 1, 2, 3, 4, 5, 6, 7], header[1:])
+#plt.legend(handles=[red,green,blue])
+plt.show()
+#print(content)
 
 
     
